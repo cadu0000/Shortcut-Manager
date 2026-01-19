514f4463787aeb8e5d2d8ea548c925ff9e894bf3
cadu0000 <ce.pinheiros1@gmail.com>
2026-01-19 19:51:40 -0300

    implement the cli support

--- src/enums/ide.rs
+++ src/enums/ide.rs
@@ -1,9 +1,22 @@
 use std::fmt;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum IDE {
     VSCode,
     JetBrains,
+}
+
+impl IDE {
+    pub fn all() -> Vec<IDE> {
+        vec![IDE::VSCode, IDE::JetBrains]
+    }
+
+    pub fn get_default_filename(&self) -> &'static str {
+        match self {
+            IDE::VSCode => "vscode_keybindings.json",
+            IDE::JetBrains => "jetbrains_keymap.xml",
+        }
+    }
 }
 
 impl fmt::Display for IDE {
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-use crate::models::default::{Shortcut, ShortcutConfig};
-use crate::models::vscode::VsCodeShortcutConfig;
-use crate::services::service::{
-    default_to_vscode, generate_shortcut_config_json, default_to_jetbrains
-};
 use crate::enums::input_device::InputDevice;
 use crate::ui::render_cli::run;
 
@@ -10,32 +5,8 @@ pub mod models;
 pub mod ui;
 pub mod enums;
 pub mod services;
+pub mod utils;
 
 fn main() {
     run();
-    let shortcut_save = Shortcut::new("Ctrl+S", "save_document", None, InputDevice::Keyboard);
-
-    let shortcut_reload = Shortcut::new("F5", "refresh_view", Some("browser".to_string()), InputDevice::Keyboard);
-
-    let shortcut_exit = Shortcut::new("Alt+F4", "exit_application", None, InputDevice::Keyboard);
-
-    let mut config = ShortcutConfig::new();
-
-    config.add_shortcut(shortcut_save);
-    config.add_shortcut(shortcut_reload);
-    config.add_shortcut(shortcut_exit);
-
-    let default_json = generate_shortcut_config_json(config.clone());
-
-    println!("{}", serde_json::to_string_pretty(&default_json).unwrap());
-    let vscode_config: VsCodeShortcutConfig = default_to_vscode(default_json.clone());
-
-    let vscode_json = generate_shortcut_config_json(vscode_config.clone());
-    println!("{}", serde_json::to_string_pretty(&vscode_json).unwrap());
-
-    let jetbrains = default_to_jetbrains(default_json.clone());
-    println!(
-        "{}",
-        serde_json::to_string_pretty(&jetbrains).unwrap()
-    );
 }
